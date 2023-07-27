362341a54efa994b34f3b47f9525b820c1ece3f7
Accpick <tech2@accpick.co.za>
2023-07-27 12:05:04 +0200

    work

--- base/urls.py
+++ base/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home_page, name="home"),
+    path('event/<str:pk>/', views.event_page, name="event"),
+    path('reg_confirmation/<str:pk>/', views.reg_confirmation, name="confirm"),
+    
 ]
--- base/views.py
+++ base/views.py
@@ -6,5 +6,18 @@ from .models import User, Event, Submission
 def home_page(request):
     users = User.objects.filter(project_participant=True)
     events = Event.objects.all()
-    context = {'user':users, 'events':events}
+    context = {'users':users, 'events':events}
     return render(request, 'home.html', context)
+
+
+def event_page(request, pk):
+    event = Event.objects.get(id=pk)
+    context = {'event':event}
+    return render(request, 'event.html', context)
+
+
+
+def reg_confirmation(request, pk):
+    event = Event.objects.get(id=pk)
+    context = {'event':event}
+    return render(request, 'event_confirmation.html', context)
